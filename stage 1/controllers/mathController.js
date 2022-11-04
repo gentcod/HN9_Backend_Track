a8d4ad3fa6f7918d77deda2c9832b12572b6061e
Oyefule <drelanorgent@gmail.com>
2022-11-04 04:16:51 +0100

    Request callback update

--- stage 1/controllers/mathController.js
+++ stage 1/controllers/mathController.js
@@ -2,20 +2,20 @@ exports.mathOperation = (req, res) => {
    const data = req.body;
 
    //Convert X and Y to Numbers; for cases where X and/or Y are sent as string
-   const x = +data.x;
-   const y = +data.y;
+   const x = Number(data.x);
+   const y = Number(data.y);
 
    //Initialize variable to hold the result of the operation
    let result;
 
    //Check for math operation
-   if (data.operation_type.value === "+" || data.operation_type.value === "add" || data.operation_type.value === "addition") result = x + y;
-   if (data.operation_type.value === "*" || data.operation_type.value === "multiply" || data.operation_type.value === "multiplication") result = x * y;
-   if (data.operation_type.value === "/" || data.operation_type.value === "divide" || data.operation_type.value === "division") result = x / y;
+   if (data.operation_type === "+" || data.operation_type === "add" || data.operation_type === "addition") result = x + y;
+   if (data.operation_type === "*" || data.operation_type === "multiply" || data.operation_type === "multiplication") result = x * y;
+   if (data.operation_type === "/" || data.operation_type === "divide" || data.operation_type === "division") result = x / y;
 
    res.status(200).json({
       slackUsername: "gentcod",
       result: result,
-      operation_type: data.operation_type.value,
+      operation_type: data.operation_type,
    })
 };
